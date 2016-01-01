bfc297f9585ad6d4ab824b44c5e5d6bf33cd5c22
U-ROBOT\Abhishek <abhicoders@gmail.com>
2016-01-02 01:26:09 +0530

    Learning curves

--- Home Insurance/scripts/helper.py
+++ Home Insurance/scripts/helper.py
@@ -2,6 +2,8 @@ from sklearn.cross_validation import StratifiedShuffleSplit
 from sklearn.preprocessing import LabelEncoder
 from sklearn.grid_search import RandomizedSearchCV
 
+from collections import defaultdict
+
 
 import pandas as pd
 
@@ -55,3 +57,35 @@ def cv_optimize(X, y, cv, clf, parameters):
 	rs.fit(X, y)
 
 	return rs
+
+def transform_for_ranked(preds, index):
+	ranks = []
+
+	for i, pred in enumerate(preds):
+		ranks.append((index[i], pred))
+
+	return ranks
+
+
+def ranked_averaging(predictions):
+	all_ranks = defaultdict(list)
+
+	for i, preds in enumerate(predictions):
+		individual_ranks = []
+
+		for e, pred in enumerate(preds):
+			individual_ranks.append( (float(pred[1]), e, pred[0]) )
+
+		for rank, item in enumerate( sorted(individual_ranks) ) :
+			all_ranks[(item[1], item[2])].append(rank)
+
+	average_ranks = []
+
+	for k in sorted(all_ranks):
+		average_ranks.append((sum(all_ranks[k])/len(all_ranks[k]),k))
+
+	ranked_ranks = []
+
+	for rank, k in enumerate(sorted(average_ranks)):
+		ranked_ranks.append((k[1][0],k[1][1],(rank * 1.)/(len(average_ranks)-1)))
+	return sorted(ranked_ranks)
