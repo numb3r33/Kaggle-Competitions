12ef138078bf51e53a552029caa7ac7e4cb2dacf
U-ROBOT\Abhishek <abhicoders@gmail.com>
2016-02-09 01:30:08 +0530

    Back to square one, text based features are not able to improve score

--- Home-Depot/scripts/features.py
+++ Home-Depot/scripts/features.py
@@ -1,12 +1,15 @@
 from sklearn.base import BaseEstimator
 from sklearn.feature_extraction import stop_words
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_selection import SelectKBest, f_regression
+
 
 import nltk
 
 import pandas as pd
 import numpy as np
+
+import re
 
 class FeatureTransformer(BaseEstimator):
 	"""
@@ -30,39 +33,30 @@ class FeatureTransformer(BaseEstimator):
 
 	def fit_transform(self, X, y=None):
 		
-		# X['search_terms'] = [' '.join(['q' + word for word in terms.split()]) for terms in X['search_term'] ]
+		X['search_term'] = X['search_term'].map(self._remove_stopwords)
+		X['search_term'] = X['search_term'].map(self._stem_words)
 
-		corpus = X.apply(lambda x: '%s %s' %(x['search_terms'], x['product_description']), axis=1)
-		words_lower = self._preprocess(corpus)
+		X['product_title'] = X['product_title'].map(self._remove_stopwords)
+		X['product_title'] = X['product_title'].map(self._stem_words)
 		
-		# empty_analyzer = lambda x: x
-		# self.unigram_vect = TfidfVectorizer(analyzer=empty_analyzer, ngram_range=(1, 1), min_df=3)
-		self.count_vect = CountVectorizer(analyzer='word', ngram_range=(1, 2))
-		bow = self.count_vect.fit_transform(words_lower)
-		# unigrams = self.unigram_vect.fit_transform(words_lower)
+		X['product_description'] = X['product_description'].map(self._remove_stopwords)
+		X['product_description'] = X['product_description'].map(self._stem_words)
 
+		# corpus = X.apply(lambda x: '%s %s %s' %(x['search_term'], x['product_title'], x['product_description']), axis=1)
+		# self.count_vect = CountVectorizer(analyzer='word', min_df=2)
+		# bow = self.count_vect.fit_transform(corpus)
 
-		# X['search_term'] = X['search_term'].map(self._remove_stopwords)
-		# X['search_term'] = X['search_term'].map(self._stem_words)
-
-		# X['product_title'] = X['product_title'].map(self._remove_stopwords)
-		# X['product_title'] = X['product_title'].map(self._stem_words)
 		
-		# X['product_description'] = X['product_description'].map(self._remove_stopwords)
-		# X['product_description'] = X['product_description'].map(self._stem_words)
-
 		is_query_in_title = X.apply(lambda x: self._contains_query_term(x['search_term'], x['product_title']), axis=1).reshape(-1, 1)
 		is_query_in_description = X.apply(lambda x: self._contains_query_term(x['search_term'], x['product_description']), axis=1).reshape(-1, 1)
 		query_length = self._get_query_length(X['search_term'])
 
-		self.truncated_svd = TruncatedSVD(n_components=200)
-		# reduced_features = self.truncated_svd.fit_transform(unigrams)
-		reduced_features = self.truncated_svd.fit_transform(bow)
+		# self.selector = SelectKBest(f_regression, k=10)
+		# reduced_features = self.selector.fit_transform(bow.todense(), X['relevance'])
 
+		
 		features = []
-		features.append(reduced_features)
-		# features.append(unigrams.toarray())
-		# features.append(bow.toarray())
+		# features.append(reduced_features)
 		features.append(is_query_in_title)
 		features.append(is_query_in_description)
 		features.append(query_length)
@@ -71,17 +65,12 @@ class FeatureTransformer(BaseEstimator):
 
 		return features
 
-	def _preprocess(self, corpus):
-		corpus = list(corpus)
-		words_lower = [' '.join([q.lower() for q in query.split(' ') if q not in self.stopwords]) for query in corpus ]
-		return words_lower
-
 	def _stem_words(self, sentence):
-		return ' '.join([self.stemmer.stem(words) for words in sentence])
+		return ' '.join([self.stemmer.stem(words) for words in sentence.split()])
 	
 	def _remove_stopwords(self, sentence):
-		return ' '.join([words.strip().lower() for words in sentence if words.strip().lower() not in self.stopwords])
-
+		return ' '.join([re.sub(r'[^\w\s\d]','',word.lower()) for word in sentence.split() if word not in self.stopwords])
+		
 	def _get_query_length(self, search_terms):
 		query_length = search_terms.map(lambda x: len(x.split(' ')))
 
@@ -93,34 +82,29 @@ class FeatureTransformer(BaseEstimator):
 	
 		
 	def transform(self, X):
-		# X['search_terms'] = [' '.join(['q' + word for word in terms.split()]) for terms in X['search_term'] ]
+		X['search_term'] = X['search_term'].map(self._remove_stopwords)
+		X['search_term'] = X['search_term'].map(self._stem_words)
+
+		X['product_title'] = X['product_title'].map(self._remove_stopwords)
+		X['product_title'] = X['product_title'].map(self._stem_words)
 		
-		corpus = X.apply(lambda x: '%s %s' %(x['search_terms'], x['product_description']), axis=1)
-		words_lower = self._preprocess(corpus)
-		bow = self.count_vect.transform(words_lower)
-		# unigrams = self.unigram_vect.transform(words_lower)
+		X['product_description'] = X['product_description'].map(self._remove_stopwords)
+		X['product_description'] = X['product_description'].map(self._stem_words)
 
-		# X['search_term'] = X['search_term'].map(self._remove_stopwords)
-		# X['search_term'] = X['search_term'].map(self._stem_words)
 
-		# X['product_title'] = X['product_title'].map(self._remove_stopwords)
-		# X['product_title'] = X['product_title'].map(self._stem_words)
+		# corpus = X.apply(lambda x: '%s %s %s' %(x['search_term'], x['product_title'], x['product_description']), axis=1)
+		# bow = self.count_vect.transform(corpus)
 		
-		# X['product_description'] = X['product_description'].map(self._remove_stopwords)
-		# X['product_description'] = X['product_description'].map(self._stem_words)
-
 
 		is_query_in_title = X.apply(lambda x: self._contains_query_term(x['search_term'], x['product_title']), axis=1).reshape(-1, 1)
 		is_query_in_description = X.apply(lambda x: self._contains_query_term(x['search_term'], x['product_description']), axis=1).reshape(-1, 1)
 		query_length = self._get_query_length(X['search_term'])
 		
 
-		reduced_features = self.truncated_svd.transform(bow)
+		# reduced_features = self.selector.transform(bow.todense())
 
 		features = []
-		features.append(reduced_features)
-		# features.append(unigrams.toarray())
-		# features.append(bow.toarray())
+		# features.append(reduced_features)
 		features.append(is_query_in_title)
 		features.append(is_query_in_description)
 		features.append(query_length)
