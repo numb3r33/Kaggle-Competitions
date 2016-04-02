351a3525edca36930b8bb7929f647720fcbff036
Abhishek <abhicoders@gmail.com>
2016-04-02 23:57:51 +0530

    Class to create different types of dataset

--- Santander-Customer-Satisfaction/scripts/feature_analysis.py
+++ Santander-Customer-Satisfaction/scripts/feature_analysis.py
@@ -12,40 +12,50 @@ import seaborn as sns
 
 # load train and test set
 
-train = pd.read_csv('./data/train.csv')
-test = pd.read_csv('./data/test.csv')
+train = pd.read_csv('./data/train.csv', index_col='ID')
+test = pd.read_csv('./data/test.csv', index_col='ID')
 
-# Feature Analysis
+## Class that would represent different synthesized datasets
 
-# 4. imp_op_var39_comer_ult1 ( needs log transformation )
-train['log_imp_op_var39_comer_ult1'] = train.imp_op_var39_comer_ult1.map(np.log1p)
-sns.FacetGrid(train[train.imp_op_var39_comer_ult1>0.0], hue='TARGET', size=4)\
-.map(sns.kdeplot, 'log_imp_op_var39_comer_ult1')\
-.add_legend();
-
-# 5. log_imp_op_var39_comer_ult3 ( needs log transformation )
-train['log_imp_op_var39_comer_ult3'] = train.imp_op_var39_comer_ult3.map(np.log1p)
-sns.FacetGrid(train[train.imp_op_var39_comer_ult3>0.0], hue='TARGET', size=4)\
-.map(sns.kdeplot, 'log_imp_op_var39_comer_ult3')\
-.add_legend();
-
-# 6. imp_op_var40_comer_ult1 ( log transform )
-train['log_train.imp_op_var40_comer_ult1'] = train.imp_op_var40_comer_ult1.map(np.log1p)
-sns.FacetGrid(train[train.imp_op_var40_comer_ult1>0.0], hue='TARGET', size=4)\
-.map(sns.kdeplot, 'log_imp_op_var40_comer_ult1')\
-.add_legend();
-
-# 7. imp_op_var40_comer_ult3
-train['log_imp_op_var40_comer_ult3'] = train.imp_op_var40_comer_ult3.map(np.log1p)
-sns.FacetGrid(train[train.imp_op_var40_comer_ult3>0.0], hue='TARGET', size=4)\
-.map(sns.kdeplot, 'log_imp_op_var40_comer_ult3')\
-.add_legend();
-
-# 8. imp_op_var40_efect_ult1
-train['log_imp_op_var40_efect_ult1'] = train.imp_op_var40_efect_ult1.map(np.log1p)
-
-sns.FacetGrid(train[train.imp_op_var40_efect_ult1>0.0], hue='TARGET', size=4)\
-.map(sns.kdeplot, 'log_imp_op_var40_efect_ult1')\
-.add_legend();
-
-# 9. 
+class Dataset():
+    def __init__(self, train, test):
+        self.train = train.copy()
+        self.test = test.copy()
+        self.features = train.columns[:-1]
+    
+    def impute_missing_values(self, strategy):
+        missing_values = [-999999.0, 9999999999.0]
+        
+        for col in self.features:
+            if (self.train[col] == missing_values[0]).any():  
+                self.train['is_missing_%s' %(col)] = (self.train[col] == missing_values[0]).astype(int)                
+                self.train[col] = self.train[col].replace(missing_values[0], strategy(self.train[col]))
+                
+                self.test['is_missing_%s' %(col)] = (self.test[col] == missing_values[0]).astype(int)                
+                self.test[col] = self.test[col].replace(missing_values[0], strategy(self.test[col]))
+            
+            elif (self.train[col] == missing_values[1]).any():
+                self.train['is_missing_%s' %(col)] = (self.train[col] == missing_values[1]).astype(int)
+                self.train[col] = self.train[col].replace(missing_values[1], strategy(self.train[col]))
+                
+                self.test['is_missing_%s' %(col)] = (self.test[col] == missing_values[1]).astype(int)
+                self.test[col] = self.test[col].replace(missing_values[1], strategy(self.test[col]))
+    
+    def log_transformation(self):
+        
+        self.train = self.train[self.features].applymap(np.log1p)
+        self.test = self.test[self.features].applymap(np.log1p)
+    
+    def discretize(self):
+        self.train = self.train[self.features].astype(np.int)
+        self.test = self.test[self.features].astype(np.int)
+        
+    def preprocess(self, impute_strategy):
+        self.impute_missing_values(impute_strategy)
+        self.log_transformation()
+        self.discretize()
+        
+        
+                
+            
+                
